4c6ffed02a08ce46ea8437e2a9e85f6f6bf53d92
codecraftlearning <codecraftlearning@gmail.com>
2026-05-20 22:40:46 +0530

    JS 1st Class

---
+++ js/index.js
@@ -0,0 +1,62 @@
+
+var num = undefined;
+/*
+Type of data => 
+
+
+Number => 1, 0, 1.2, 1.257, -1, -6666, -5.88 etc
+Text / String =>  "anything", 'any letter', 'a', "b", ' ', " ", "", '' etc 
+Object => 
+    {
+        name: 'ankit',
+        age: '32',
+        gender: 'male'
+        .
+        .
+        .
+    }
+
+Array / List => [1,2,3,4,5,6,7,8,9], ['aasdas', 'sdfsdfd', 'sdfsdfsd', 'sdfdsfdsf', 'werwe'], [1, 'asdasd', 32.6, {}]
+*/
+
+// var num1 = 5;
+// var num2 = 2;
+
+// var sum = num1 + num2;
+// var mul = num1 * num2;
+// var div = num1 / num2;
+// var rem = num1 % num2;
+// var pow = num1 ** num2;
+
+// console.log("Sum = " + sum);
+// console.log("Multiplication = " + mul);
+// console.log("Division = " + div);
+// console.log("Reminder = " + rem);
+// console.log("Power = " + pow);
+
+
+
+// var firstName = "ankit";
+// var lastName = "soni";
+
+// var fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+
+
+
+console.log(num);  //This is possible due to variable HOISTING
+num = 10;
+num = 20;
+
+let val = 10;
+val = 20;
+console.log(val);
+
+const data = 10;
+console.log(data);
+
+//Homework:  
+// Write the differences b/w var, let and const
+// Write about Hoiting or What is hoisting?
