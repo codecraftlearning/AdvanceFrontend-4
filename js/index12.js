1d4979dc8e24afe9b92ea87b07a68ad3f4baaab1
codecraftlearning <codecraftlearning@gmail.com>
2026-06-07 12:17:37 +0530

    logic and homework

---
+++ js/index12.js
@@ -0,0 +1,30 @@
+// write a program to print the sum of all digits in a number;
+//eg: if number is 1234 then print 1+2+3+4 = 10;
+
+// 1234 % 4 = 4
+// 1234 / 10 = 123.4
+//123 % 10 = 3
+//123 / 10 = 12.3
+//12 % 10 = 2
+//12 / 10 = 1.2
+// 1 % 10 = 1
+// 1 / 10 = 0.1
+
+function sumOfDigits(num) {
+  let sum = 0;
+
+  while (num > 0) {
+    let rem = num % 10;
+    sum += rem;
+    num = parseInt(num / 10);
+  }
+  return sum;
+}
+
+console.log(sumOfDigits(123456789));
+
+
+// write a program to reverse a number;
+//eg if num = 12345 then after reversing num should become 54321; and print it
+//if any one not tried working on this logic,
+//class will be cancelled for that day. (and will be counted too!!)
