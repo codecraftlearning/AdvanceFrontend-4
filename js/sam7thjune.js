48274fa004f48d4be799cea212d790f835e6f3ae
codecraftlearning <codecraftlearning@gmail.com>
2026-06-08 22:02:14 +0530

    fixes

--- js/sam7thjune.js
+++ js/sam7thjune.js
@@ -2,9 +2,7 @@
 //eg ; if num = 12345 then after reversing num should become 54321 and print it//
 
 function reverseNumber(num){
-    let reverse = 0;
-
-
+    let reversed = 0;
 
     while (num>0){
         let rem = num % 10;
